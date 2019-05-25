fe0feca0019c8ac55e6b3888de2995b46662d531
MaxxximD <43450521+MaxxximD@users.noreply.github.com>
2019-05-25 23:38:15 +0300

    add task 2

---
+++ DIALOGS(WPF)/DIALOGS(WPF)/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DIALOGS_WPF_
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string Password { get; set; }
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Password = "qwerty";
+            passwordBox1.PasswordChar = '*';
+           
+
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (passwordBox1.Password == Password)
+            {
+                Window1 win1 = new Window1(Password);
+                win1.Owner = this;
+                win1.ShowDialog();
+               
+            }
+            else
+                MessageBox.Show("Неверный пароль!");
+             
+        }
+
+       
+
+        private void PasswordBox1_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            
+
+        }
+
+    }
+}
---
+++ DIALOGS(WPF)/DIALOGS(WPF)/Window1.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DIALOGS_WPF_
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        
+        public Window1(string p)
+        {
+            InitializeComponent();
+            passwordBox1.Password = p;
+            passwordBox2.Password = p;
+            passwordBox1.PasswordChar = '*';
+            passwordBox2.PasswordChar = '*';
+
+
+        }
+
+        //Кнопка OK
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = Owner as MainWindow;
+            main.Password = passwordBox1.Password;
+            DialogResult = true;
+            MessageBox.Show("Пароль изменён!");
+            
+            
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void PasswordBox1_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (passwordBox1.Password == passwordBox2.Password && passwordBox1.Password != "")
+                button1.IsEnabled = true;
+            else
+                button1.IsEnabled = false;
+        }
+
+        private void PasswordBox2_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (passwordBox1.Password == passwordBox2.Password && passwordBox1.Password != "")
+                button1.IsEnabled = true;
+            else
+                button1.IsEnabled = false;
+
+        }
+
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            Hide();
+        }
+
+        
+    }
+}
